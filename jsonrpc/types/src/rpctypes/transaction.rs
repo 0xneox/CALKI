d21a7862a0eaccac1f7def4335468b654f581e7f
jiksz <31536713+jiksz@users.noreply.github.com>
2017-11-19 14:10:49 +0530

    todo

---
+++ jsonrpc/types/src/rpctypes/transaction.rs
@@ -0,0 +1,77 @@
+// CALKI
+// Copyright 2016-2017 Zibbit Labs.
+
+// This program is free software: you can redistribute it
+// and/or modify it under the terms of the GNU General Public
+// License as published by the Free Software Foundation,
+// either version 3 of the License, or (at your option) any
+// later version.
+
+// This program is distributed in the hope that it will be
+// useful, but WITHOUT ANY WARRANTY; without even the implied
+// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+// PURPOSE. See the GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+use bytes::Bytes;
+use libproto::FullTransaction as PTransaction;
+use libproto::blockchain::SignedTransaction as ProtoSignedTransaction;
+use protobuf::Message;
+use util::H256;
+use util::U256;
+
+// TODO: No need Deserialize. Just because test in trans.rs
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+pub struct FullTransaction {
+    pub hash: H256,
+    pub content: Bytes,
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+pub struct RpcTransaction {
+    pub hash: H256,
+    pub content: Bytes,
+    #[serde(rename = "blockNumber")]
+    pub block_number: U256,
+    #[serde(rename = "blockHash")]
+    pub block_hash: H256,
+    pub index: U256,
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+#[serde(untagged)]
+pub enum BlockTransaction {
+    Full(FullTransaction),
+    Hash(H256),
+}
+
+impl From<PTransaction> for RpcTransaction {
+    fn from(mut ptransaction: PTransaction) -> Self {
+        let stx = ptransaction.take_transaction();
+        let mut bhash: H256 = H256::default();
+        bhash.0.clone_from_slice(ptransaction.block_hash.as_slice());
+
+        let unverified_tx = stx.get_transaction_with_sig();
+        let tx = unverified_tx.get_transaction();
+        trace!("GET ProtoTransaction: nonce {:?}, block_limit {:?}, data {:?}, quota {:?}, to {:?}", tx.get_nonce(), tx.get_valid_until_block(), tx.get_data(), tx.get_quota(), tx.get_to());
+
+        RpcTransaction {
+            hash: H256::from_slice(stx.get_tx_hash()),
+            content: Bytes(unverified_tx.write_to_bytes().unwrap()),
+            block_number: U256::from(ptransaction.block_number),
+            block_hash: bhash,
+            index: U256::from(ptransaction.index),
+        }
+    }
+}
+
+impl From<ProtoSignedTransaction> for FullTransaction {
+    fn from(stx: ProtoSignedTransaction) -> Self {
+        FullTransaction {
+            hash: H256::from_slice(stx.get_tx_hash()),
+            content: Bytes(stx.get_transaction_with_sig().write_to_bytes().unwrap()),
+        }
+    }
+}
