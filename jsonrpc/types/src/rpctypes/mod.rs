859a39d5383d332a4cab9bbc663d3c269b6c33e8
jiksz <31536713+jiksz@users.noreply.github.com>
2017-11-19 14:12:47 +0530

    Create mod.rs

---
+++ jsonrpc/types/src/rpctypes/mod.rs
@@ -0,0 +1,39 @@
+// CALKI
+// Copyright 2016-2017 Zibbit Labs.
+
+// This program is free software: you can redistribute it
+// and/or modify it under the terms of the GNU General Public
+// License as published by the Free Software Foundation,
+// either version 3 of the License, or (at your option) any
+// later version.
+
+// This program is distributed in the hope that it will be
+// useful, but WITHOUT ANY WARRANTY; without even the implied
+// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+// PURPOSE. See the GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+pub mod receipt;
+pub mod log;
+pub mod block_number;
+pub mod call_request;
+pub mod filter;
+pub mod transaction;
+pub mod block;
+pub mod middle_modle;
+pub mod index;
+pub mod proof;
+
+pub use self::block::*;
+pub use self::block_number::*;
+pub use self::call_request::*;
+pub use self::filter::*;
+pub use self::index::Index;
+pub use self::log::*;
+pub use self::middle_modle::*;
+pub use self::proof::*;
+pub use self::receipt::*;
+pub use self::transaction::*;
