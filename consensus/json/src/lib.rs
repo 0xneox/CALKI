0bea532ef165f2bc2085121f5a163d3208692d2d
Gokul Ravindran <neohex262@gmail.com>
2017-11-19 19:03:20 +0530

    Create lib.rs

---
+++ consensus/json/src/lib.rs
@@ -0,0 +1,39 @@
+// CALKI
+// Copyright 2016-2017 Zibbit Labs.
+
+// This program is free software: you can redistribute it
+// and/or modify it under the terms of the GNU General Public
+// License as published by the Free Software Foundation,
+// either version 3 of the License, or (at your option) any
+// later version.
+
+// This program is distributed in the hope that it will be
+// useful, but WITHOUT ANY WARRANTY; without even the implied
+// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+// PURPOSE. See the GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#![allow(unused_extern_crates)]
+#[macro_use]
+extern crate serde_derive;
+extern crate serde;
+extern crate serde_json;
+extern crate calki_crypto as crypto;
+extern crate util;
+
+mod engine;
+mod authority_round;
+mod tendermint;
+mod spec;
+
+
+pub use self::authority_round::*;
+pub use self::engine::*;
+pub use self::spec::*;
+pub use self::tendermint::*;
+
+
+#[test]
+fn it_works() {}
