293a33bf6f16149bd912154e9a1f2ac03df131bb
jiksz <31536713+jiksz@users.noreply.github.com>
2017-11-19 14:22:33 +0530

    Create lib.rs

---
+++ jsonrpc/types/src/lib.rs
@@ -0,0 +1,51 @@
+// CALKI
+// Copyright 2016-2017 Zibbit Labs.
+
+// This program is free software: you can redistribute it
+// and/or modify it under the terms of the GNU General Public
+// License as published by the Free Software Foundation,
+// either version 3 of the License, or (at your option) any
+// later version.
+
+// This program is distributed in the hope that it will be
+// useful, but WITHOUT ANY WARRANTY; without even the implied
+// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+// PURPOSE. See the GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+#![allow(unused_extern_crates)]
+extern crate libproto;
+extern crate protobuf;
+extern crate uuid;
+#[macro_use]
+extern crate log;
+extern crate util;
+#[macro_use]
+extern crate serde_derive;
+extern crate bincode;
+extern crate serde_json;
+extern crate serde;
+extern crate rustc_serialize;
+extern crate proof;
+extern crate common_types as types;
+extern crate calki_crypto as crypto;
+
+
+mod id;
+mod params;
+pub mod error;
+pub mod bytes;
+pub mod request;
+pub mod response;
+pub mod rpctypes;
+pub mod method;
+pub use self::error::*;
+pub use self::id::*;
+pub use self::params::*;
+pub use self::request::RpcRequest;
+pub use serde_json::Value;
+pub use serde_json::to_string;
+pub use serde_json::value::to_value;
