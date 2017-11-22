3055933c20632ef9f219157f333bef0eaaa4550a
Gokul Ravindran <neohex262@gmail.com>
2017-11-23 02:53:22 +0530

    Create main.rs

---
+++ chain/main.rs
@@ -0,0 +1,131 @@
+// CALKI
+// Copyright 2016-2017 Zibbit Labs.
+
+// This program is free software: you can redistribute it
+// and/or modify it under the terms of the GNU General Public
+// License as published by the Free Software Foundation,
+// either version 3 of the License, or (at your option) any
+// later version.
+
+// This program is distributed in the hope that it will be
+// useful, but WITHOUT ANY WARRANTY; without even the implied
+// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+// PURPOSE. See the GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#![allow(unused_must_use, unused_assignments)]
+extern crate core;
+#[macro_use]
+extern crate log;
+extern crate libproto;
+extern crate pubsub;
+extern crate util;
+extern crate clap;
+extern crate dotenv;
+extern crate logger;
+extern crate jsonrpc_types;
+extern crate common_types as types;
+extern crate byteorder;
+extern crate serde_json;
+extern crate protobuf;
+extern crate error;
+extern crate proof;
+
+pub mod forward;
+
+use clap::App;
+use core::db;
+use core::libchain;
+use core::libchain::Genesis;
+use forward::Forward;
+use pubsub::start_pubsub;
+use std::fs::File;
+use std::io::BufReader;
+use std::sync::Arc;
+use std::sync::mpsc::channel;
+use std::thread;
+use std::time;
+use std::time::Duration;
+use util::datapath::DataPath;
+use util::kvdb::{Database, DatabaseConfig};
+use util::panichandler::set_panic_handler;
+
+fn main() {
+    dotenv::dotenv().ok();
+
+    // Always print backtrace on panic.
+    ::std::env::set_var("RUST_BACKTRACE", "full");
+
+    //exit process when panic
+    set_panic_handler();
+
+    logger::init();
+    info!("CALKI:chain");
+    let matches = App::new("chain")
+        .version("0.1")
+        .author("Cryptape")
+        .about("CALKI Block Chain Node powered by Rust")
+        .arg_from_usage("-g, --genesis=[FILE] 'Sets a genesis config file")
+        .arg_from_usage("-c, --config=[FILE] 'Sets a switch config file'")
+        .get_matches();
+
+    let mut genesis_path = "genesis";
+    if let Some(ge) = matches.value_of("genesis") {
+        trace!("Value for genesis: {}", ge);
+        genesis_path = ge;
+    }
+
+    let mut config_path = "config";
+    if let Some(c) = matches.value_of("config") {
+        trace!("Value for config: {}", c);
+        config_path = c;
+    }
+
+    let (tx, rx) = channel();
+    let (ctx_pub, crx_pub) = channel();
+    start_pubsub("chain", vec!["net.blk", "net.sync", "consensus.blk", "jsonrpc.request", "auth.blk_tx_hashs_req", "consensus.msg"], tx, crx_pub);
+
+    let nosql_path = DataPath::nosql_path();
+    trace!("nosql_path is {:?}", nosql_path);
+    let config = DatabaseConfig::with_columns(db::NUM_COLUMNS);
+    let db = Database::open(&config, &nosql_path).unwrap();
+    let genesis = Genesis::init(genesis_path);
+    let config_file = File::open(config_path).unwrap();
+    let chain = Arc::new(libchain::chain::Chain::init_chain(Arc::new(db), genesis, BufReader::new(config_file)));
+
+    let block_tx_hashes = chain.block_tx_hashes(chain.get_current_height()).expect("shoud return current block tx hashes");
+    chain.delivery_block_tx_hashes(chain.get_current_height(), block_tx_hashes, &ctx_pub);
+
+    let (write_tx, write_rx) = channel();
+    let forward = Forward::new(chain.clone(), ctx_pub, write_tx);
+    forward.broad_current_status();
+    let forward_write = forward.clone();
+    //chain 读写分离
+    //chain 读数据 => 查询数据
+    thread::spawn(move || loop {
+                      if let Ok((key, msg)) = rx.recv() {
+                          forward.dispatch_msg(key, msg);
+                      }
+                  });
+
+    //chain 写数据 => 添加块
+    thread::spawn(move || loop {
+                      if let Ok(blocks) = write_rx.recv_timeout(Duration::new(8, 0)) {
+                          forward_write.dispatch_blocks(blocks);
+                      }
+                      forward_write.broad_current_status();
+                  });
+
+    //garbage collect
+    let mut i: u32 = 0;
+    loop {
+        thread::sleep(time::Duration::from_millis(10000));
+        if i > 100 {
+            chain.collect_garbage();
+            i = 0;
+        }
+        i += 1;
+    }
+}
